257794671208338f6799b1738f30a6f299cbdfe8
Catt3a <169023485+Catt3a@users.noreply.github.com>
2026-06-30 03:47:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-// Раздаём статику (HTML страница)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Маршрут для скачивания файла
 
 app.get('/download', (req, res) => {
   const file = path.join(__dirname, 'files', 'extensionn.crx'); // замени на свой файл
